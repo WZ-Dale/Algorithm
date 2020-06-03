3b5beb426a6dd704be0d9820bbd2e4cbc1a35b1e
WZ-Dale <w18829898295@163.com>
2020-06-03 10:39:53 +0800

    二进制手表的两种解答方法：回溯 和 二进制破解

--- BT_Read_Binary_Watch.cpp
+++ BT_Read_Binary_Watch.cpp
@@ -2,69 +2,67 @@
 来源：力扣（LeetCode）
 链接：https://leetcode-cn.com/problems/binary-watch
 */
-#include <iostream>
-#include <vector>
-#include <string>
 
-using namespace std;
-
-class Solution
-{
+class Solution {
 public:
-    void backTrace(int num, vector<string>& ret, string h, string m, int curDepth){
-        //边界，找到一种组合，判定是否合法，放入数组中，结束此路径，向上回溯
-        if(curDepth == num){
-            if(h.empty()){
-                h = "0";
-            }
-            if(m.empty()){
-                m = "00";
-            }
-            if(atoi(h.c_str()) > 11 || atoi(m.c_str()) > 59){
+    void backtrack(int num,int start,pair<int,int>& time)
+    {
+        if(num==0)
+        {
+            if(time.first>11||time.second>59)//判断合法性
                 return;
-            }
-            ret.push_back(h+":"+m);
+            string temp_hour=to_string(time.first);
+            string temp_minute=to_string(time.second);
+            if(temp_minute.size()==1)//如果minute只有一位要补0
+                temp_minute.insert(0,"0");
+            res.push_back(temp_hour+":"+temp_minute);//构造格式
             return;
-        }       
-        //遍历每一种可能的组合
-        
+        }
+    
+        for(int i=start;i<10;i++)
+        {
+            if(time.first>11||time.second>59)
+                continue;
+            pair<int,int>store=time;//保存状态
+            if(i<4)
+                time.first+=hash[i];
+            else
+                time.second+=hash[i];
+            backtrack(num-1,i+1,time);//进入下一层，注意下一层的start是i+1，即从当前灯的下一盏开始
+            time=store;//恢复状态
+        }
     }
-    vector<string> readBinaryWatch(int num){
-        vector<string> ret;
-        backTrace(num, ret, "", "", 0);
-        return ret;
+    vector<string> readBinaryWatch(int num) {
+        pair<int,int>time(0,0);//初始化时间为0:00
+        backtrack(num,0,time);
+        return res;
     }
 private:
-    vector<vector<int>> watch = {{1, 2, 4, 8}, {1, 2, 4, 8, 16, 32}};
+    vector<string>res;
+    unordered_map<int,int> hash={{0,1},{1,2},{2,4},{3,8},{4,1},{5,2},{6,4},{7,8},{8,16},{9,32}};
 };
-void print_v(vector<string>& v){
-    for(auto& e : v){
-        cout << e << ", ";
+
+class Solution {
+public:
+    //计算二进制中1的个数
+    int count1(int n) {
+        int res = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            res++;
+        }
+        return res;
     }
-    cout << endl;
-
-    //以下是不打印最后一个逗号的两种写法
-
-    // int n = v.size();
-    // for(int i = 0; i < n; ++i){
-    //     if(i == n - 1){
-    //         cout << v[i];
-    //         break;
-    //     }
-    //     cout << v[i] << ", ";
-    // }
-    // cout << endl;
-
-    // for(int i = 0; i < n - 1; ++i){
-    //     cout << v[i] << ", ";
-    // }
-    // cout << v[n - 1] << endl;
-}
-
-int main(){
-    int num = 1;
-    Solution S;
-    vector<string> v = S.readBinaryWatch(num);
-    print_v(v);
-    return 0;
-}
+    vector<string> readBinaryWatch(int num) {
+        vector<string> res;
+        //直接遍历  0:00 -> 12:00   每个时间有多少1
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 60; j++) {
+                if (count1(i) + count1(j) == num) {
+                    res.push_back(to_string(i)+":"+ (j < 10 ? "0"+to_string(j) : to_string(j)));
+                }
+            }
+        }
+        return res;
+    }
+};
